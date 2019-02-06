0743f8e30baab5f3fb816c45f862903f4ef99c06
Zandew <39287272+Zandew@users.noreply.github.com>
2019-02-06 14:38:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,20 +14,20 @@ def main():
     print("ROUND 1 BEGIN")
     for i in range(5):
         print("ROUND 1 QUESTION "+str(i+1))
-        questions += 1
-        if (round1.r1()):
+        if (round1.r1(score, question)):
             score += 1
+        question += 1
     print("ROUND 2 BEGIN")
     for i in range(5):
         print("ROUND 1 QUESTION "+str(i+1))
-        questions += 1
-        if (round2.r2()):
+        if (round2.r2(score, question)):
             score += 1
+        questions += 1
     print("ROUND 3 BEGIN")
     for i in range(5):
         print("ROUND 1 QUESTION "+str(i+1))
-        questions += 1
-        if (round3.r3()):
+        if (round3.r3(score question)):
             score += 1
+        questions += 1
     
 main()
