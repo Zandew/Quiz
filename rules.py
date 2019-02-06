37d40ef2c9139a6f5bf956b5cb784828d67c71b7
Jasonnnnnnnnnnn <wqc1127@gmail.com>
2019-02-06 15:04:34 -0500

    Update rules.py

--- rules.py
+++ rules.py
@@ -4,5 +4,6 @@ def rules():
     print("The questions and the descriptions for each round is going to be given to you,")
     print("and you only get 1 attempt to answer the question correctly!\n")
     print("Input your answer on the line right after the question")
-    print("or input \"s\" to see your current score.\n")    
+    print("or input \"s\" to see your current score.")    
+    print("or input \"r\" to see the rules again.\n")
  
