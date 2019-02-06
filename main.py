a75322746a4df63ec5c4bcd430a891ed282b98df
Jasonnnnnnnnnnn <wqc1127@gmail.com>
2019-02-06 14:34:03 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import round1, round2, round3, random, math
+import round1, round2, round3, random, math, rules
 
 def main():
-    round1.r1(1, 2)
+    print("Welcome to the FUN MATH QUIZ!\n")
+    print("There will be 3 rounds in total and 5 questions in each round:")
+    print("Round 1:\t Multiply & Divide")
+    print("Round 2:\t Solving Quadratic Equations")
+    print("Round 3:\t Solving Right Triangles\n")
+    rules.rules()
+    print("ARE YOU READY TO START??(Enter anything to start the game)")
+    input()
+    round1.r1()
     
 main()
