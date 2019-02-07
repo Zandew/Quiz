7b54d6dd31411d5913e349b36d2e2029f985c087
Zandew <andewzxue@gmail.com>
2019-02-06 22:01:27 -0500

    y

--- round3.py
+++ round3.py
@@ -62,7 +62,7 @@ def r3(cor, tot):
              rules.rules()
         elif userAns == "S" or userAns == "s":
             print("Your current score is "+str(cor)+" out of "+str(tot))
-            print("You are currently on question #"+str((tot)%5+1)+" on round #"+str(math.ceil((tot+1)/5)))
+            print("You are currently on question #"+str((tot)%5+1)+" on round #"+str(math.ceil((tot+1)/5))+"\n")
         else:
             print("Your input is invaild! Please try again.\n")
         draw()
