b0f28e8beeae92817c76e4b8f9e5906df2c015aa
Zandew <39287272+Zandew@users.noreply.github.com>
2019-02-07 11:29:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import round1, round2, round3, random, math, rules
 def main():
     print("Welcome to the FUN MATH QUIZ!\n")
     print("There will be 3 rounds in total and 5 questions in each round:")
-    print("Round 1:\t Multiply & Divide")
-    print("Round 2:\t Solving Quadratic Equations")
-    print("Round 3:\t Solving Right Triangles\n")
+    print("Round 1:\tMultiply & Divide")
+    print("Round 2:\tSolving Quadratic Equations")
+    print("Round 3:\tSolving Right Triangles\n")
     rules.rules()
     print("ARE YOU READY TO START??(Enter anything to start the game)")
     input()
@@ -13,19 +13,19 @@ def main():
     questions = 0
     print("ROUND 1 BEGIN")
     for i in range(5):
-        print("ROUND 1 QUESTION "+str(i+1))
+        print("ROUND 1 QUESTION "+str(i+1)+"\n")
         if (round1.r1(score, questions)):
             score += 1
         questions += 1
     print("ROUND 2 BEGIN")
     for i in range(5):
-        print("ROUND 2 QUESTION "+str(i+1))
+        print("ROUND 2 QUESTION "+str(i+1)="\n")
         if (round2.r2(score, questions)):
             score += 1
         questions += 1
     print("ROUND 3 BEGIN")
     for i in range(5):
-        print("ROUND 3 QUESTION "+str(i+1))
+        print("ROUND 3 QUESTION "+str(i+1)+"\n")
         if (round3.r3(score, questions)):
             score += 1
         questions += 1
