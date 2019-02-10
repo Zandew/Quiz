de839c4d84dfe3cd240cb6a32baafdf92b0a81b8
albertchan58 <47067136+albertchan58@users.noreply.github.com>
2019-02-10 12:28:27 -0500

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,9 +1,14 @@
+'''
+Parameter: No parameters
+Purpose:   Outputs the rules to the user
+Return:    None
+'''
+
 def rules():
-    print("\nRULES")
+    print("RULES")
     print("You will be given a point for each question you solved.\n")
     print("The questions and the descriptions for each round is going to be given to you,")
     print("and you only get 1 attempt to answer the question correctly!\n")
     print("Input your answer on the line right after the question")
     print("or input \"s\" to see your current score.")    
     print("or input \"r\" to see the rules again.\n")
- 
