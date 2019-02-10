8fd8277b2b575415a27bb0b63d9a6a308326ca1e
Jasonnnnnnnnnnn <wqc1127@gmail.com>
2019-02-10 12:37:00 -0500

    Update rules.py

--- rules.py
+++ rules.py
@@ -5,10 +5,14 @@ Return:    None
 '''
 
 def rules():
+    print("\n"+"-"*70)
     print("RULES")
     print("You will be given a point for each question you solved.\n")
-    print("The questions and the descriptions for each round is going to be given to you,")
-    print("and you only get 1 attempt to answer the question correctly!\n")
-    print("Input your answer on the line right after the question")
-    print("or input \"s\" to see your current score.")    
-    print("or input \"r\" to see the rules again.\n")
+    print("The questions and the descriptions for each round is going to be given to you.")
+    print("You ONLY get 1 attempt to answer the question correctly!\n")
+    print("For each correct answer, you will get 1 point!")
+    print("The total number of points possible is 15.\n")
+    print("Follow the instuctions and input your answer on the line right after each question,")
+    print("or input \"s\" to see your current score,")    
+    print("or input \"r\" to see the rules again.")
+    print("-"*70,"\n")
