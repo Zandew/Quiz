0cd22ce46c9bd7f47fe353972ec067db80a54786
Jasonnnnnnnnnnn <wqc1127@gmail.com>
2019-02-10 12:16:17 -0500

    Update rules.py

--- rules.py
+++ rules.py
@@ -1,5 +1,5 @@
 def rules():
-    print("RULES")
+    print("\nRULES")
     print("You will be given a point for each question you solved.\n")
     print("The questions and the descriptions for each round is going to be given to you,")
     print("and you only get 1 attempt to answer the question correctly!\n")
