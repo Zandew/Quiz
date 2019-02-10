2e267f171eb240281e66f440c8b519d4f3bd5048
albertchan58 <47067136+albertchan58@users.noreply.github.com>
2019-02-10 12:01:29 -0500

    Update round1.py

--- round1.py
+++ round1.py
@@ -1,5 +1,22 @@
 import random, rules, math
 
+'''
+Parameter: The user's current score and the total number of problems asked so far are
+           passed as arguments to dynamically display the score whenever the
+           enters "S" or "s". The user will see their final score at
+           the end.
+Purpose:   This function prompts the user to find the product of a multiplication
+           equation (x*y) or the quotient of a division equation (x/y). The two randomly
+           given integers (x and y) are guaranteed to the integers. Thus, the answer must
+           only be an integer. This function determines the ability of the user to find
+           the answer to a multiplication or division equation. After each question the
+           user will be notified whether they answered the question correctly or not. This
+           function will allow the user see an up-to-date score or rules after each
+           question if he/she wants to.
+Return:    This function returns True if user answers correctly, but
+           False otherwise.
+'''
+
 def r1(cor, tot):
         
     x,y = random.randint(2,100),random.randint(2,20)
@@ -26,7 +43,7 @@ def r1(cor, tot):
             print("Congratulations! You are correct!\n")
             return True
         else:
-            print("You are wrong! The correct answer is",int(x/y),"\n")
+            print("You are wrong! The correct answer is",str(int(x/y)) + ".","\n")
             return False
     elif md == 0:
         x,y = random.randint(2,20),random.randint(2,20)
@@ -51,6 +68,5 @@ def r1(cor, tot):
             print("Congratulations! You are correct!\n")
             return True
         else:
-            print("You are wrong! The correct answer is",x*y,"\n")
+            print("You are wrong! The correct answer is",str(x*y) + ".","\n")
             return False
-
